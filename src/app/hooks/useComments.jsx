56d1a05cd61159f2fb01c7133eb5bbda58e6055e
Imran-72 <petrow318@gmail.com>
2022-01-16 14:13:13 +0300

    Добавление  метода создания комментария

--- src/app/hooks/useComments.jsx
+++ src/app/hooks/useComments.jsx
@@ -1,5 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { nanoid } from "nanoid";
+import { useParams } from "react-router-dom";
+import { useAuth } from "./useAuth";
 
 const CommentsContext = React.createContext();
 
@@ -8,13 +11,26 @@ export const useComments = () => {
 };
 
 export const CommentsProvider = ({ children }) => {
+    const { userId } = useParams();
+    const { currentUser } = useAuth();
     const [comments, setComments] = useState([]);
 
     useEffect(() => {
         setComments(null);
     }, []);
+
+    async function createComment(data) {
+        const comment = {
+            ...data,
+            pageId: userId,
+            created_at: Date.now(),
+            _id: nanoid(),
+            userId: currentUser._id
+        };
+        console.log(comment);
+    }
     return (
-        <CommentsContext.Provider value={{ comments }}>
+        <CommentsContext.Provider value={{ comments, createComment }}>
             {children}
         </CommentsContext.Provider>
     );
